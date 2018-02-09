d00a7339f9cb4096e857e4fbc258d384bdde9d5c
Dominick Han <dominick.han@mail.utoronto.ca>
2018-02-09 13:35:14 -0500

    Can choose which sensor to use in WebEndpoint

--- types/WebEndpoint.js
+++ types/WebEndpoint.js
@@ -1,7 +1,8 @@
 module.exports = function(config) {
+	sensors = config.sensor ? [config.sensor.substring(0, 1).toUpperCase()] : ['T', 'H'];
 	return {
 		config: config,
-		sensors: ['T', 'H'],
+		sensors: sensors,
 		process: function (body) {
 			return {T: JSON.parse(body).temperature, H: JSON.parse(body).humidity}
 		}
